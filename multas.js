0fd235659f83a51c289defb4d1302180813e0ced
Ana Clara <anacsds1@gmail.com>
2026-02-17 20:43:42 -0300

    config

--- multas.js
+++ multas.js
@@ -5,10 +5,10 @@ function ver() { //função que vai verificar velocidade
                             // .trim() remove espaços em branco
     res.innerHTML = `Sua velocidade atual é de <strong>${vel}</strong> Km/h` //o que vai ser escrito dentro da div 'res'
     if (vel >= 60) {
-        res.innerHTML = `Você está <strong>MULTADO</strong> por excesso de velocidade!`
+        res.innerHTML += `, você está <strong>MULTADO</strong> por excesso de velocidade!`
     }
     else if (vel === "")
         res.innerHTML = `Digite sua velocidade atual!`
     else if (vel < 60)
-    res.innerHTML = `Está tudo certo, continue dirigindo com atenção ao trânsito!`
+    res.innerHTML += `, está tudo certo, continue dirigindo com atenção ao trânsito!`
 }
